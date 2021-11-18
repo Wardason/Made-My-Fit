93f1d43c65104ac77672517ab6e024aef6b82485
Leonard <66140015+Wardason@users.noreply.github.com>
2021-11-18 19:13:10 +0100

    Rounded Button as component with constructor

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:made_my_fit/components/rounded_button.dart';
 import 'package:made_my_fit/screens/login_screen.dart';
 import 'package:made_my_fit/screens/registration_screen.dart';
 
@@ -13,38 +15,39 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text(
-          'Welcome Screen',
-        ),
-      ),
       body: SizedBox.expand(
         child: Container(
           alignment: Alignment.topCenter,
           decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage("assets/Start_Screen.PNG"),
-                fit: BoxFit.cover),
+                image: AssetImage("assets/Test.jpg"), fit: BoxFit.cover),
           ),
-          child: Column(
-            children: <Widget>[
-              OutlineButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
-                child: Text('Login'),
-              ),
-              OutlineButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RegistrationScreen.id);
-                },
-                child: Text('Registration'),
-              ),
-            ],
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 100.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                RoundedButton(
+                  title: 'Log In',
+                  colour: Colors.black54,
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                ),
+                RoundedButton(
+                  title: 'Sign Up',
+                  colour: Colors.black54,
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// login or registration to welcome screen use Navigator.pushReplacementNamed
